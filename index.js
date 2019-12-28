2e72244c749d79dee6798ae0bdcbf090a032d442
mboudakian <michelle.boudakian@gmail.com>
2019-12-28 03:47:00 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+document.querySelector("#play").addEventListener("click", randomize);
+
+function randomize() {
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏆Player 1 Wins!🏆";
+  } else if (randomNumber2 === randomNumber1) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  } else {
+    document.querySelector("h1").innerHTML = "🏆Player 2 Wins!🏆";
+  }
+}
